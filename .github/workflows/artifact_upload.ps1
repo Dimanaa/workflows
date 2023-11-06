caad9ceba36685db7092d10c21fee6f8476b951e
Dimana Stoyanova Stoyanova <DIST@milestone.dk>
2023-11-06 14:17:46 +0200

    testing file upload

--- .github/workflows/artifact_upload.ps1
+++ .github/workflows/artifact_upload.ps1
@@ -1,6 +1,5 @@
-$firebaseProject = "workflows-7be96"
 $storageBucket = "workflows-7be96.appspot.com"
-$localFilePath = ".github/workflows/ConsoleApp1/ConsoleApp1/bin/Debug/ConsoleApp1.exe" 
+$localFilePath = ".github/workflows/ConsoleApp1/ConsoleApp1/bin/Debug/test.txt" 
 $firebaseToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTY5OTAxNzM2MiwiZXhwIjoxNjk5MDIwOTYyLCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay1nNmxib0B3b3JrZmxvd3MtN2JlOTYuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzdWIiOiJmaXJlYmFzZS1hZG1pbnNkay1nNmxib0B3b3JrZmxvd3MtN2JlOTYuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJ1aWQiOiJmaXJlYmFzZS1hZG1pbnNkay1nNmxib0B3b3JrZmxvd3MtN2JlOTYuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20ifQ.j7plO3w8r03DoWbIOe13E6_zKExTd4QZBzHVcDsqEgps7suOv0oSzsepPTqU0iKCpmHoVkqWqOITc6XBe0JjUsvdRfrXajWdh7UDbEIvBrV6Sx4fIqPHIDTKAIsai9j7WYN67DrI_ZT4vuRAxR609Wvq_N2OPzyOnKZ-p4Luk3WDQuzc-nIk1XM9mLnfvofqQgA9X9Og8_T_Ph19MMCRP5MJkAtWwJeQgapHtRnoZEF2thID3qzBnCuDSUGOTNRdGyTwhqR0URF02XllzN3_kNjbSga5CerTSryEdGNJZ-598ERGqsLtl6fGxgj1pZFbBBF1PWiK2EcoP9fuSO49TQ"
 
 # Function to upload file to Firebase Cloud Storage
@@ -28,7 +27,7 @@ function UploadToFirebaseStorage {
     # JSON body for the upload request
     $body = @{
         "name" = $fileName
-        "contentType" = "application/octet-stream"
+        "contentType" = "text/plain"
         "data" = $fileBase64
     } | ConvertTo-Json
 
